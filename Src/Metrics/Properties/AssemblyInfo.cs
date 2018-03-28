1a5ace7f903bb3321f98e17e2a9a4c7c6304723a
Alkampfergit <ricci.gianmaria@gmail.com>
2018-03-28 13:16:20 +0200

    Pushed nuget packages

--- Src/Metrics/Properties/AssemblyInfo.cs
+++ Src/Metrics/Properties/AssemblyInfo.cs
@@ -2,3 +2,5 @@
 
 [assembly: AssemblyTitle("Metrics.NET")]
 [assembly: AssemblyDescription("Metrics monitoring and reporting library")]
+[assembly: AssemblyVersion("0.5.5.1000")]
+[assembly: AssemblyFileVersion("0.5.5.1000")]
